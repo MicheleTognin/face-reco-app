a23a6643f7ca7a5c2b3376d9c29201150204bd5d
MicheleTognin <60986445+MicheleTognin@users.noreply.github.com>
2020-05-04 19:34:56 +0200

    deploy

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Navigation = () => {
+ return ( 
+   <div style={{display: 'flex', justifyContent: 'center'}}>
+  <nav>
+    <p className='f3 black pa3'><h1>Face Recognition App</h1></p>
+  </nav>
+   </div>
+ )
+}
+
+export default Navigation;
